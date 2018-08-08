6eb4959a4b0a4316bba7a0d071954e1f8cf12ead
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-08-08 16:38:26 +0300

    Added the C# ApprovalTests

---
+++ CSHARP/Refactoring/Tests/MockDispenser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringKungFu
+{
+    public class MockDispenser : IDispenser
+    {
+        private string log;
+        public Ingredient GetIngredient(IngredientTypes ingredient, Places place)
+        {
+            log +="GetIngredient: " +
+                ingredient.ToString() +
+                " place: " +
+                place.ToString();
+
+            return new Ingredient(false);
+        }
+
+        public Ingredient GetPasta(PastaType pasta, Places place)
+        {
+            log+= "GetPlace: " +
+               pasta.ToString() +
+               " place: " +
+               place.ToString();
+
+            return new Ingredient(true);
+        }
+
+        public override string ToString()
+        {
+            return log;
+        }
+    }
+}
--- CSHARP/Refactoring/Tests/Tests.cs
+++ CSHARP/Refactoring/Tests/Tests.cs
@@ -1,15 +1,43 @@
 ﻿using ApprovalTests;
+using ApprovalTests.Reporters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace RefactoringKungFu
 {
+    [UseReporter(typeof(DiffReporter))]
     [TestClass]
     public partial class PastaMakerTests
     {
+        List<Dish> Examples = new List<Dish>
+        {
+            new Dish(SauceType.Alfredo, PastaType.FreshSpaghetti),
+            new Dish(SauceType.Bolognese, PastaType.FreshSpaghetti),
+            new Dish(SauceType.Marinara, PastaType.FreshSpaghetti),
+            new Dish(SauceType.Pesto, PastaType.FreshSpaghetti),
+            new Dish(SauceType.Alfredo, PastaType.Ravioly),
+            new Dish(SauceType.Bolognese, PastaType.Ravioly),
+            new Dish(SauceType.Marinara, PastaType.Ravioly),
+            new Dish(SauceType.Pesto, PastaType.Ravioly),
+        };
+
 
         [TestMethod]
         public void PastaMakerTest()
         {
+            {
+                MockDispenser dispenser = new MockDispenser();
+                PastaMaker maker = new PastaMaker(dispenser);
+                string output = "";
+                foreach (var dish in Examples)
+                {
+                    output += dish.ToString();
+                    maker.Cook(dish.sauce, dish.pasta);
+                }
+
+                output += dispenser.ToString();
+                Approvals.Verify(output);
+            }
         }
     }
 }
